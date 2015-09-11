43a887cdd3304aa8ef7071c32fc0717349ad9908
Anshul Thakur <anshulthakur@rediffmail.com>
2015-09-11 20:32:28 +0530

    Regex matches output of frequency scan using rtl_sdr

--- misc/file_read_regex.py
+++ misc/file_read_regex.py
@@ -10,10 +10,11 @@ if len(sys.argv) < 0:
 
 filename = sys.argv[1]
 fd = open(filename, 'r')
-
-line_regex = re.compile(r'\w\s+\w\s(?P<scan_freq>\d+)\s+(?P<rx_power>\d+)\s.*$')
+#2015-09-03 08:27:33.588183 center_freq 460900000.0 freq 460000000.0 power_db 5.59471050376 noise_floor_db -78.5314550789
+line_regex = re.compile(r'.*center_freq\s+(?P<scan_freq>\d+\.\d+)\s+.*power_db\s+(?P<rx_power>\d+\.\d+)\s.*$')
 rx_power = []
 scan_freq = []
+freq = 0
 i = 0
 while 1:
 	line = fd.readline()
@@ -22,9 +23,15 @@ while 1:
 	line_match = line_regex.match(line)
 	if line_match is not None:
 		#Extract the patterns of Received Power and Frequency
-		rx_power[i] = line_match.group('rx_power')
-		scan_freq[i] = line_match.group('scan_freq')
-		i++
+		rx_power.append(float(line_match.group('rx_power')))
+		scan_freq.append(float(line_match.group('scan_freq')))
+		if freq > scan_freq[-1]:
+			scan_freq.pop()
+			rx_power.pop()
+			break
+		freq = scan_freq[-1]
+		print('{freq}\t{power}\n'.format(power=rx_power[-1], freq=scan_freq[-1]))
+		i+=1
 plt.plot(scan_freq, rx_power)
 
 plt.show()

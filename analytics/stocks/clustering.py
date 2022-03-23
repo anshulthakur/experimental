73f6cc365ba5f5285d082ea8b5453b49ec4ed543
Anshul Thakur <athakur@cdot.in>
2022-03-23 16:35:33 +0530

    Daily data read into HDFstore

--- analytics/stocks/clustering.py
+++ analytics/stocks/clustering.py
@@ -46,7 +46,6 @@ def get_stock_listing(stock):
 
 def get_stock_listing(stock):
     import datetime
-    print(stock.sid)
     first_date = datetime.date(2020, 1, 1)
     last_date = datetime.date(2020, 12, 31)
     listing = Listing.objects.filter(stock=stock, date__range=(first_date, last_date))
@@ -57,21 +56,31 @@ def get_stock_listing(stock):
     df = df.sort_index()
     df = df.reindex(columns = ['closing'])
     df.index = pd.to_datetime(df.index)
-    #Resample weekly
-    logic = {'closing' : 'last',
-             }
-    #Resample on weekly levels
-    df_weekly = df.resample('W').apply(logic)
-    df_weekly.index -= to_offset("6D")
 
-    #df_weekly.rename(columns={"closing":stock.sid.replace(' ', '_')}, inplace=True)
-    df_weekly.rename(columns={"closing":stock.security}, inplace=True)
+    #Delete duplicate columns
+    #df = df.loc[:,~df.columns.duplicated()]
+    df.drop_duplicates(inplace = True)
+    resample = False
+    if resample:
+        #Resample weekly
+        logic = {'closing' : 'last',
+                 }
+        #Resample on weekly levels
+        df_weekly = df.resample('W').apply(logic)
+        df_weekly.index -= to_offset("6D")
+
+        #df_weekly.rename(columns={"closing":stock.sid.replace(' ', '_')}, inplace=True)
+        df_weekly.rename(columns={"closing":stock.security}, inplace=True)
+        df = df_weekly
+    else:
+        df.rename(columns={"closing":stock.security}, inplace=True)
 
     #Optionally, filter out by date range
     #start_date = '2020-01-01'
     #end_date = '2021-12-31'
     #df = df.loc[start_date:end_date]
-    return df_weekly.T
+    print(f'{stock.sid}: {df.shape}')
+    return df.T
 
 
 
@@ -88,9 +97,9 @@ except:
 count = 0
 #store = pd.HDFStore('store.h5')
 
-chunk_size = 500
+chunk_size = 5000
 
-path = 'store.h5'
+path = 'store_daily.h5'
 df = pd.DataFrame()
 if len(read_list) < len(stocks):
     chunk = (len(read_list)//chunk_size)+1
@@ -103,6 +112,7 @@ for stock in stocks:
         for sid in read_list:
             fd.write('{}\n'.format(sid))
         fd.close()
+        print(df.head())
         with pd.HDFStore(path) as store:
             print('Save chunk_{}'.format(chunk))
             #store['chunk_{}'.format(chunk)] = df  # save it
@@ -114,6 +124,14 @@ for stock in stocks:
     if stock.name != 'NIFTY' and stock.name != 'BANKNIFTY':
         sdf = get_stock_listing(stock)
         if len(sdf>0):
+            seen = set()
+            dupes = [x for x in sdf.columns if x in seen or seen.add(x)]
+            if len(dupes)>0:
+                print(f'Duplicated: {dupes}')
+                print(sdf[dupes[0]])
+            #print(f'DF shape: {df.shape}\tDF columns: {len(df.columns)}({len(set(df.columns))})\
+#\tSDF columns: {len(sdf.columns)}({len(set(sdf.columns))})\tUnion:{len(set(df.columns).union(set(sdf.columns)))}')
+
             df = df.append(sdf)
             #df = pd.concat([df, sdf])
             count += 1
@@ -148,4 +166,4 @@ with pd.HDFStore(path) as store:
     df = store['df']
     df = df.T
     store['df'] = df
-    print(df.head())
+

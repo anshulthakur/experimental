d1ff00e936b3ec87154777928dad7739432f8d93
anshulthakur <anshulthakurjourneyendless@gmail.com>
2022-03-30 08:16:47 +0530

    Missed Market model

---
+++ analytics/stocks/stocks/models/market.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Market(models.Model):
+    name = models.CharField(blank=False, 
+                            null=False,
+                            max_length=255)
+    
+    def __str__(self):
+        return self.name
+

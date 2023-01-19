f02084c6393521da6b10b09e1fbdea918123f12f
anshulthakur <anshulthakurjourneyendless@gmail.com>
2023-01-19 22:47:53 +0530

    [WIP] Multiple indicators from single node. Concat version

--- analytics/stocks/tradebot/nodes/indicators.py
+++ analytics/stocks/tradebot/nodes/indicators.py
@@ -2,6 +2,7 @@ from tradebot.base import FlowGraphNode
 import talib as ta
 from talib.abstract import *
 from lib.logging import log
+import pandas as pd
 
 class IndicatorNode(FlowGraphNode):
     def __init__(self, indicators=[], **kwargs):
@@ -74,9 +75,30 @@ class Indicator(FlowGraphNode):
             return
         df = kwargs.get('data')
         log(f'{self}: {df.tail(0)}', 'debug')
+        '''
+        Here, we add a multi-level index in order to support multiple indicators in a single node.
+        This is like:
+
+        Input: 
+
+        datetime   |        stock_1          |           stock_2           |
+         <>        |        1234             |           4567              |
+        Output:
+
+                   |        stock_1        |          stock_2      |
+        datetime   | close   ind_1   ind_2 | close   ind_1   ind_2 |
+         <>        | 1234     x_1     y_1  | 4567     x_2     y_2  |
+
+        We could totally have multiple nodes of a single level index and support only a single 
+        indicator per node, but that would involve unnecessary copying of inputs to that many nodes.
+
+        What we can do is, if there is only a single column, we can drop the level, but that would 
+        make the design a bit convoluted
+        '''
+        #df.columns = pd.MultiIndex.from_product([df.columns, ['close']])
         #https://stackoverflow.com/questions/40225683/how-to-simply-add-a-column-level-to-a-pandas-dataframe
         for indicator in self.indicators:
-            s_df = self.indicators[indicator]['method'](df[self.indicators[indicator]['column']], **self.indicators[indicator]['attributes'])
+            s_df = df.apply(lambda x: self.indicators[indicator]['method'](x, **self.indicators[indicator]['attributes']))
         for node,connection in self.connections:
             await node.next(connection=connection, data = s_df.tail(1))
         self.consume()

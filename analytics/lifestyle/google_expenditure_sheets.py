c73c01f980163333030ce39ffcf70acc6f637333
Anshul Thakur <athakur@cdot.in>
2023-09-07 16:01:24 +0530

    Updated code to download, clean up, and cache the data under analysis.

--- analytics/lifestyle/google_expenditure_sheets.py
+++ analytics/lifestyle/google_expenditure_sheets.py
@@ -9,6 +9,11 @@ import os
 
 import pandas as pd
 import os.path
+import traceback
+import re
+from datetime import datetime
+
+from urllib.parse import parse_qs, urlparse
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -31,51 +36,113 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 APPLICATION_NAME = 'Python Interface Exploration'
 
 class Sheets:
-    def __init__(self, creds, spreadsheet_id):
+    def __init__(self, creds, spreadsheet_id, cache_folder='cache'):
         self.creds = creds
         self.spreadsheet_id = spreadsheet_id
         self.service = build('sheets', 'v4', credentials=self.creds)
+        self.cache_folder = cache_folder
 
     def open(self, sheet_names):
         try:
+            # Create the cache folder if it doesn't exist
+            if not os.path.exists(self.cache_folder):
+                os.makedirs(self.cache_folder)
+            
             # Dictionary to store DataFrames for each sheet
             df = None
-
+            dataframes = []
             for sheet_name in sheet_names:
-                if df is None:
-                    df = self.fetch_data(sheet_name)
+                s_df = None
+                cache_file = os.path.join(self.cache_folder, f"{sheet_name}.csv")
+
+                # Check if a cached CSV file exists
+                if os.path.exists(cache_file):
+                    s_df = pd.read_csv(cache_file, index_col="Date")
+                    s_df.index = pd.to_datetime(s_df.index)
+                    print(f'Load {cache_file} from cache')
                 else:
+                    # Fetch data from Google Sheets
                     s_df = self.fetch_data(sheet_name)
-                    df = pd.concat([df, s_df], join='outer', sort=True)
-                    df.sort_index(inplace=True)
+
+                    # Save the fetched data to a CSV file for caching
+                    s_df.to_csv(cache_file)
+
+                dataframes.append(s_df)
+
+            df = pd.concat(dataframes, join='outer', sort=True)
+            df.sort_index(inplace=True)
 
             # Now, df contains the combined DataFrame for the specified sheets
             return df
 
         except Exception as e:
             print(f"Error: {str(e)}")
+            traceback.print_exc()
             return None
 
+    def get_sheet_names(self):
+        try:
+            sheet_metadata = self.service.spreadsheets().get(spreadsheetId=self.spreadsheet_id).execute()
+            sheets = sheet_metadata.get('sheets', [])
+
+            sheet_names = [sheet['properties']['title'] for sheet in sheets]
+            return sheet_names
+
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            traceback.print_exc()
+            return None
+        
     def fetch_data(self, sheet_name):
         range_name = f'{sheet_name}!A:G'  # Adjust columns as needed
         result = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range=range_name).execute()
         values = result.get('values', [])
 
+        print(f'Sheet: {sheet_name}')
         if not values:
             print(f'No data found in sheet: {sheet_name}')
             return None
+        month = sheet_name.split("'")[0]
+        year = int(sheet_name.split("'")[1])+2000
 
-        ii = 0
+        columns = ['Date','Money spent',
+                    'Expense category', 'Spent on', 
+                    'Point of Sale/App used', 'Via card/wallet/cash',
+                    'Paid by']
+        rows = []
+        end_reached = False
         for row in values:
-            if ii == 0:
-                for col in range(0, len(row)):
-                    row[col] = row[col].strip()
             if len(row) < 3:
-                break
-            ii += 1
+                if end_reached:
+                    break
+                end_reached = True #Allow one blank space between entries
+                continue
+            if row[0].strip() == 'Date':
+                #columns = [col.strip() for col in row]
+                continue
+            elif len(row) == len(columns):
+                if len(row[0].strip().split(' '))>1 and len(row[0].strip().split(' '))!=3: #Minor patchwork on date
+                    row[0] = f'{row[0].strip()} {year}'
+                print(row)
+                rows.append(row)
+            elif len(row) < len(columns):
+                #if row has entry for some category, only then enter it
+                if len(row[2].strip())>0:
+                    newrow = [col for col in row]
+                    newrow += ['' for jj in range(len(row), len(columns))]
+                    if len(newrow[0].strip().split(' '))>1 and len(newrow[0].strip().split(' '))!=3: #Minor patchwork on date
+                        newrow[0] = f'{newrow[0].strip()} {year}'
+                    print(newrow)
+                    rows.append(newrow)
+            else: #len > len(columns)
+                rows.append(row[0:len(columns)])
 
-        df = pd.DataFrame(values[1:ii], columns=values[0])
+        print(columns)
+        df = pd.DataFrame(rows, columns=columns)
 
+        # Clean and parse the date column
+        df["Date"] = df["Date"].apply(self.clean_and_parse_date)
+        #print(df.head(10))
         # Parse dates and set them as the index
         df["Date"] = pd.to_datetime(df["Date"])
 
@@ -83,8 +150,56 @@ class Sheets:
         df['Date'] = df['Date'].fillna(method='ffill')
 
         df.set_index("Date", inplace=True)
-
+        print(df.head(10))
         return df
+
+    def clean_and_parse_date(self, date_str):
+        # Define regex patterns for common date formats
+        date_patterns = [
+            r'\d{1,2} (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}',  # 27 Jan 2023
+            # Add more patterns for other date formats as needed
+        ]
+
+        # Check if the date_str matches any of the patterns
+        for pattern in date_patterns:
+            match = re.search(pattern, date_str)
+            if match:
+                matched_date = match.group()
+                # Perform further parsing and return a standardized date
+                standardized_date = self.parse_date(matched_date)
+                return standardized_date
+
+        # If no matching pattern is found, return None (or handle as needed)
+        return date_str
+
+    def parse_date(self, date_str):
+        # Implement custom logic to parse and standardize date_str
+        # You can use datetime.strptime and datetime.strftime for parsing and formatting
+
+        # Example:
+        # parsed_date = datetime.datetime.strptime(date_str, "%d %b %Y")
+        # standardized_date = parsed_date.strftime("%d %B %Y")
+        parsed_date = datetime.strptime(date_str, "%d %b %Y")
+
+        # Use strftime to format the date as '27 January 2023'
+        standardized_date = parsed_date.strftime("%d %B %Y")
+        # Return the standardized date
+        return standardized_date
+    
+def run_console_hack(flow):
+    flow.redirect_uri = 'http://localhost:1'
+    auth_url, _ = flow.authorization_url()
+    print(
+        "Visit the following URL:",
+        auth_url,
+        "After granting permissions, you will be redirected to an error page",
+        "Copy the URL of that error page (http://localhost:1/?state=...)",
+        sep="\n"
+    )
+    redir_url = input("URL: ")
+    code = parse_qs(urlparse(redir_url).query)['code'][0]
+    flow.fetch_token(code=code)
+    return flow.credentials
 
 def get_credentials():
     token = 'token.json'
@@ -98,7 +213,8 @@ def get_credentials():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                                             'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            #creds = flow.run_local_server(port=0)
+            creds = run_console_hack(flow)
         # Save the credentials for the next run
         with open(token, 'w') as tokenfile:
             tokenfile.write(creds.to_json())
@@ -107,9 +223,21 @@ def get_credentials():
 def main():
     creds = get_credentials()
     gsheet = Sheets(creds=creds, spreadsheet_id='<id>')
-    df = gsheet.open(sheet_names=["August'23", "July'23"])
-    
+    #sheet_names = gsheet.get_sheet_names()
+    sheet_names = ["September'23", "August'23", "July'23", "June'23", "May'23", "April'23",
+                    "March'23", "February'23", "January'23", "December'22", "September'22", 
+                    "August'22", "July'22", "June'22", "May'22", "April'22", "March'22", 
+                    "February'22", "January'22", "December'21", "November'21", "October'21", 
+                    "September'21", "August'21", "July'21", "Jun '21", "May'21", "April'21", 
+                    "March'21", "February'21", "January'21", "December'20", "November'20", 
+                    "October'20", "September'20", "August'20", "July'20", "June'20", "May'20", 
+                    "April'20", "March'20", "February'20", "January'20", "December'19", 
+                    "November'19", "October'19", "September'19"]
+    #print(sheet_names)
+
+    df = gsheet.open(sheet_names=sheet_names)
     print(df.head(20))
+    print(df.tail(20))
     
 
 if __name__ == "__main__":
